28a613f36fb07c4ced98cde456e65a4d0fa9a65a
Musab <mmak.lughat@gmail.com>
2025-07-07 02:10:04 +0500

    minor ui changes

--- Assign1/qoutegenerator/app/search/[topic]/page.tsx
+++ Assign1/qoutegenerator/app/search/[topic]/page.tsx
@@ -35,20 +35,25 @@ export default function TopicPage() {
 
 
     return (
-        <div className="bg-hero bg-gray-700 p-6 min-h-screen content-center font-mono justify-center w-full mx-auto bg-gray-200">
-            <div className="text-4xl font-semibold text-gray-600 text-center bg-linear-to-r from-transparent via-green-100 to-transparent rounded-xl mx-10 my-4 p-2">Quotes on {topic.charAt(0).toUpperCase() + topic.slice(1)}...</div>
-            <ScrollArea className="rounded-xl max-h-[calc(100vh-24vh)] border-collapse w-auto mx-2 px-6 py-2 ">
-            {quotes.map((quote) => (
-                <Card className="my-4 dropshadow-2xl border-2 bg-green-100 rounded-xl border-gray-100 mx-2 hover:-translate-y-1 hover:cursor-pointer hover:shadow-xl delay-150 duration-500" key={quote.id}>
-                    <CardHeader>
-                        <CardTitle className="font-bold">{quote.author}~</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p>{quote.text}</p>
-                    </CardContent>
-                </Card>
-            ))}
-            </ScrollArea>
+        <div className="bg-hero bg-gray-700 p-6 min-h-screen font-mono justify-center w-full mx-auto bg-gray-200">
+            <div className="text-4xl font-semibold text-gray-700 text-center bg-linear-to-r from-transparent via-green-100 to-transparent rounded-xl mx-10 p-2 capitalize">Quotes on {topic.charAt(0).toUpperCase() + topic.slice(1)} by {(author? author:"")}...</div>
+            <div className="flex flex-col justify-center h-[calc(100vh-20vh))]">
+                <ScrollArea className="rounded-xl max-h-[calc(100vh-24vh)] border-collapse w-auto mx-2 px-6 py-2">
+                    {quotes.map((quote) => (
+                        <Card
+                            className="my-6 dropshadow-2xl border-2 bg-green-100 rounded-xl border-2 shadow-gray-400 mx-2 hover:-translate-y-1 hover:cursor-pointer hover:shadow-md delay-150 duration-500"
+                            key={quote.id}
+                        >
+                            <CardHeader>
+                                <CardTitle className="font-bold">{quote.author}~</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>{quote.text}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </ScrollArea>
+            </div>
         </div>
     );
 }
