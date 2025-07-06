90840eeb356defe6243f7441e45300f09f00d569
Musab <mmak.lughat@gmail.com>
2025-07-06 23:56:48 +0500

    UI Changes whitespace issue fixed input form updated

--- Assign1/qoutegenerator/app/page.tsx
+++ Assign1/qoutegenerator/app/page.tsx
@@ -2,8 +2,8 @@ import { InputForm } from "@/components/search-form"
 
 export default function LoginPage() {
   return (
-    <div className="bg-radial from-gray-500 to-gray-300 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-2xl max-w-sm md:max-w-full font-mono shadow-2xl border-2 bg-green-100 rounded-xl border-gray-100 p-8 ">
+    <div className="bg-radial from-gray-600 to-gray-300  flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl font-mono border-2 bg-green-100 shadow-green-100 rounded-xl p-8 ">
         <InputForm  />
       </div>
     </div>
