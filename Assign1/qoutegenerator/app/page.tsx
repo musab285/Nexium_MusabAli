908ec94eccc375525aa3cced3580d6b699738cf0
Musab <mmak.lughat@gmail.com>
2025-07-07 01:41:43 +0500

    UI Changes

--- Assign1/qoutegenerator/app/page.tsx
+++ Assign1/qoutegenerator/app/page.tsx
@@ -2,7 +2,7 @@ import { InputForm } from "@/components/search-form"
 
 export default function LoginPage() {
   return (
-    <div className="bg-radial from-gray-600 to-gray-300  flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div className="bg-hero bg-gray-700 flex min-h-svh flex-col items-center justify-center ">
       <div className="w-full max-w-sm md:max-w-3xl font-mono border-2 bg-green-100 shadow-green-100 rounded-xl p-8 ">
         <InputForm  />
       </div>
--- Assign1/qoutegenerator/app/search/[topic]/page.tsx
+++ Assign1/qoutegenerator/app/search/[topic]/page.tsx
@@ -35,11 +35,11 @@ export default function TopicPage() {
 
 
     return (
-        <div className="bg-radial from-gray-600 to-gray-300  p-6 min-h-screen font-mono justify-center w-full mx-auto bg-gray-200">
-            <div className="text-4xl font-semibold mb-4 p-2">Quotes on {topic.charAt(0).toUpperCase() + topic.slice(1)}...</div>
-            <ScrollArea className="rounded-b-xl h-[calc(100vh-24vh)] border-collapse w-auto mx-2 px-6 py-2 ">
+        <div className="bg-hero bg-gray-700 p-6 min-h-screen content-center font-mono justify-center w-full mx-auto bg-gray-200">
+            <div className="text-4xl font-semibold text-gray-600 text-center bg-linear-to-r from-transparent via-green-100 to-transparent rounded-xl mx-10 my-4 p-2">Quotes on {topic.charAt(0).toUpperCase() + topic.slice(1)}...</div>
+            <ScrollArea className="rounded-xl max-h-[calc(100vh-24vh)] border-collapse w-auto mx-2 px-6 py-2 ">
             {quotes.map((quote) => (
-                <Card className="my-4 shadow-2xl border-2 bg-green-100 rounded-xl border-gray-100 mx-2 hover:-translate-y-1 hover:cursor-pointer hover:shadow-xl delay-150 duration-500" key={quote.id}>
+                <Card className="my-4 dropshadow-2xl border-2 bg-green-100 rounded-xl border-gray-100 mx-2 hover:-translate-y-1 hover:cursor-pointer hover:shadow-xl delay-150 duration-500" key={quote.id}>
                     <CardHeader>
                         <CardTitle className="font-bold">{quote.author}~</CardTitle>
                     </CardHeader>
